4da597f1921b1ffe912b8eeeea893fdbd390ead9
Bessonov Petr <104450684+UnDeaD72@users.noreply.github.com>
2024-10-18 19:21:21 +0400

    Исправлен код: убрано использование 'using namespace std;'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-using namespace std;  // Лишнее использование пространства имен
 
 int main() {
-    string name;  // Переменная для хранения имени пользователя
-    cout << "Enter your name: ";  // Запрос имени
-    cin >> name;  // Ввод имени
-    cout << "Hello world from " << name << "!" << endl;  // Вывод сообщения
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
