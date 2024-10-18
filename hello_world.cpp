18068071c29c220bf2b4e9d912550e0cf4703ccd
Bessonov Petr <104450684+UnDeaD72@users.noreply.github.com>
2024-10-18 19:03:50 +0400

    Программа теперь запрашивает имя пользователя и выводит персонализированное сообщение

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,9 @@
 using namespace std;  // Лишнее использование пространства имен
 
 int main() {
-    cout << "Hello, World!" << endl;  // Вывод сообщения
+    string name;  // Переменная для хранения имени пользователя
+    cout << "Enter your name: ";  // Запрос имени
+    cin >> name;  // Ввод имени
+    cout << "Hello world from " << name << "!" << endl;  // Вывод сообщения
     return 0;
 }
