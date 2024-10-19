dcb4a4dd9e0afd711d3531b9156279c98c7de6a9
Bessonov Petr <104450684+UnDeaD72@users.noreply.github.com>
2024-10-19 12:51:18 +0400

    Изменен стиль кода согласно правилам Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,6 @@
-#include <iostream>  // Подключение библиотеки ввода-вывода
+#include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;  // Вывод сообщения в консоль
-    return 0;  // Завершение программы
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
-    #include <iostream>  // Подключение библиотеки для ввода-вывода
-
-    int main() {
-        std::cout << "Hello, World!" << std::endl;  // Вывод строки в консоль
-        return 0;  // Завершение программы
-    }
