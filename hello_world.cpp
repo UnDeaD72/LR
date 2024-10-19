f889d87a47cc596929c487e80f0717f4e402621c
Bessonov Petr <104450684+UnDeaD72@users.noreply.github.com>
2024-10-19 12:52:43 +0400

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,9 +4,3 @@ int main() {
     std::cout << "Hello, World!" << std::endl;  // Вывод сообщения в консоль
     return 0;  // Завершение программы
 }
-    #include <iostream>  // Подключение библиотеки для ввода-вывода
-
-    int main() {
-        std::cout << "Hello, World!" << std::endl;  // Вывод строки в консоль
-        return 0;  // Завершение программы
-    }
